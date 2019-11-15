1e3540e267a1685c82751ac52d5924f976f04847
Ayman <aymaprod12@gmail.com>
2019-11-15 20:01:40 +0100

    premeir commit après avoir eu une grosse freieur

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+    const load = document.getElementById("loader")
+    load.style.transform="translateY(-100%)";
+})
+
+var btn = document.querySelectorAll(".hamburger")
+var nav = document.querySelectorAll(".nav")
+
