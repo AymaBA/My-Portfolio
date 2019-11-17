0502fd98e2b0faad776f0962b4093b545ed351f8
Ayman <aymaprod12@gmail.com>
2019-11-17 20:02:20 +0100

    ajout des cartes de compétence

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-window.addEventListener("load", function(){
-    const load = document.getElementById("loader")
-    load.style.transform="translateY(-100%)";
-})
+try {
+    window.addEventListener("load", function(){
+        const load = document.getElementById("loader")
+        load.style.transform="translateY(-100%)";
+        console.log("Terminé");
+    })
+} catch{
+    console.log("error");
+} finally{
+    console.log("Terminé");
+    
+}
+// window.addEventListener("load", function(){
+//     const load = document.getElementById("loader")
+//     load.style.transform="translateY(-100%)";
+// })
 
 var btn = document.querySelectorAll(".hamburger")
 var nav = document.querySelectorAll(".nav")
